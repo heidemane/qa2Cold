efb33f109f4e3664a002b1cfa31a86c9598e6dc6
Laura <laura.heidemane@gmail.com>
2018-10-10 23:22:46 +0300

    initial commit

--- src/test/java/HomeWrk.java
+++ src/test/java/HomeWrk.java
@@ -2,22 +2,26 @@ import org.junit.jupiter.api.Test;
 
 
 public class HomeWrk {
-            private String text = "Interest to pay for 10 years with ";
+    private String text = "Interest to pay for 10 years";
 
-            @Test
-            public void loanPercent() {
-                interestToPay(20000,10);
-                interestToPay(20000,8);
-                interestToPay(20000,6);
+    @Test
+    public void calculateLoanPayment () {
 
-            }
+        int loanAmount = 1000;
+        int termInYears = 10;
+        double interestRate = 0.1;
+        double interestRate2 = 0.08;
+        double interestRate3 = 0.06;
 
-            private void interestToPay(Integer a, Integer b) {
-                Integer  c = a * b / 100;
+        double toPay1 = loanAmount * (1 + termInYears * interestRate);
+        double toPay2 = loanAmount * (1 + termInYears * interestRate2);
+        double toPay3 = loanAmount * (1 + termInYears * interestRate3);
+        double toPayTotal = toPay1+toPay2+toPay3;
 
-                System.out.println(text + b + "% of " + a + " is: " + c);
-
+        System.out.println(text + toPay1);
+        System.out.println(toPay2);
+        System.out.println(toPay3);
+        System.out.println("Tolal amount to pay" + " " + toPayTotal);
     }
-
 
 }
