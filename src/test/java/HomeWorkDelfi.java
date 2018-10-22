066e0b8495f00e2df9fcde2e3cecf7bb8e5eb9ff
Heidemane <Laura.heidemane@gmail.com>
2018-10-22 16:49:27 +0300

    Total piece of art

--- src/test/java/HomeWorkDelfi.java
+++ src/test/java/HomeWorkDelfi.java
@@ -5,42 +5,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.awt.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class HomeWorkDelfi {
 
-private final By ARTICLE_TITLE = By.xpath(".//a[@class = 'top2012-title']");
-//    //private final By MOBILE_ARTICLE_TITLE = By.xpath(".//a[@class= 'md-scrollpos']");
-@Test
-public void homeWorkDelfi () {
-    System.setProperty("webdriver.gecko.driver", "c:/geckodriver.exe");
-    WebDriver driver = new FirefoxDriver();
-    driver.manage().window().maximize();
-    driver.get("http://www.delfi.lv");
+    private final By ARTICLE_TITLE = By.xpath(".//a[@class = 'top2012-title']");
+    private final By MOBILE_ARTICLE_TITLE = By.xpath(".//a[@class= 'md-scrollpos']");
 
-    WebElement article = driver.findElement(ARTICLE_TITLE);
+    @Test
+    public void homeWorkDelfi() {
+        System.setProperty("webdriver.gecko.driver", "c:/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.delfi.lv");
+        WebElement article = driver.findElement(ARTICLE_TITLE);
+        String articleText1 = article.getText();
 
-        List<String> webDelfiarticles = new ArrayList<String>();
+        String articleText = article.getText();
+        driver.manage().window().maximize();
+        driver.get("http://m.delfi.lv");
+        WebElement article = driver.findElement(MOBILE_ARTICLE_TITLE);
+        String articleText2 = article.getText();
 
-        webDelfiarticles.add("5G tīkls Latvijā: kas to būvēs un kas par to maksās ");
-        webDelfiarticles.add("Bordāns sola panākt ZZS atstāšanu opozīcijā");
-        webDelfiarticles.add("'Lielais Kristaps 2018'. Balso par labākajām pašmāju filmām!");
-        webDelfiarticles.add("Izstāšanās no Līguma par raķešu likvidāciju ir bezatbildīga, uzskata Gorbačovs");
-        webDelfiarticles.add("Kas ir 'Tropu Tramps' Bolsonaru: 5 provokatīvas atziņas un plāni");
-
-        List<WebElement> articles = driver.findElements(ARTICLE_TITLE);
+        Assertions.assertEquals(articleText1, articleText2, "Fatal Laura`s error!");
 
 
-        for (int i = 0; i < 5; i++) {
-            Assertions.assertEquals(webDelfiarticles.get(i), articles.get(i).getText(), "Title Nr." + (i+1) + "is not correct");
-
-//        pageOneValues.add(article.getText());
-//
-//        driver.get("http://m.delfi.lv/");
-//
-//        WebElement mob_article = driver.findElement(MOBILE_ARTICLE_TITLE);
-//        Assertions.assertEquals(pageOneValues.get(0), mob_article.getText(), "DISASTER");
-//
+    }
 }
-}}
