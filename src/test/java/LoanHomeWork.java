be642b7a8938104f0c81b3691a0a7bfb7e83ebea
Laura <laura.heidemane@gmail.com>
2018-10-23 22:19:18 +0300

    initial commit

--- src/test/java/LoanHomeWork.java
+++ src/test/java/LoanHomeWork.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class LoanHomeWork {
     private double amountClientNeeds = 3000;
-    private double amountToPay = 16200;
+    private double amountToPay = 5400;
 
     @Test
     public void loanHomeWork() {
@@ -13,9 +13,9 @@ public class LoanHomeWork {
         double sumFor20Years;
         double sumFor30Years;
 
-        sumFor10Years = (amountClientNeeds * (1 + 10 * 0.1));
-        sumFor20Years = (amountClientNeeds * (1 + 10 * 0.08));
-        sumFor30Years = (amountClientNeeds * (1 + 10 * 0.06));
+        sumFor10Years = (amountClientNeeds/3 * (1 + 10 * 0.1));
+        sumFor20Years = (amountClientNeeds/3 * (1 + 10 * 0.08));
+        sumFor30Years = (amountClientNeeds/3 * (1 + 10 * 0.06));
 
         totalSumToPay = sumFor10Years + sumFor20Years + sumFor30Years;
 
